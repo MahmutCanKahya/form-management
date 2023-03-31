4f76532a3fd96002d3c94e91b03ea76e784e3bd4
Mahmut Can Kahya <mahmutcankahya@windowslive.com>
2023-03-31 10:30:34 +0300

    base url added

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import FormPage from "./pages/FormPage";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/form-management">
           <Routes>
             <Route path="/">
               <Route path="/forms" element={<HomePage />} />
